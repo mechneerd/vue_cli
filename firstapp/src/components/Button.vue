d3215cd137df0ef2c9ea52f691e6a06305314282
prasanth <prasanth_ramaraju@outlook.com>
2024-01-16 16:40:23 +0530

    first commit

---
+++ firstapp/src/components/Button.vue
@@ -0,0 +1,18 @@
+<template>
+    <button @click="newfun()" class="btn" :style="{background:color}">{{text}}</button>
+</template>
+
+<script>
+export default {
+ name:'Button',
+ props:{
+    text:String,
+    color:String,
+ },
+ methods:{
+    newfun(){
+        console.log('okay')
+    }
+ }
+}
+</script>
